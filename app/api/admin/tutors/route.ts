7b51c7418bfb0483d92d6c238df57f55601658c1
vant11dung <vantiendung08012007@gmail.com>
2026-06-05 01:10:21 +0700

    fix: ep vercel generate prisma truoc khi build

--- app/api/admin/tutors/route.ts
+++ app/api/admin/tutors/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma"; // Thêm dấu {} vào đây
 
 // 1. Lấy danh sách hồ sơ đang chờ duyệt
 export async function GET() {
