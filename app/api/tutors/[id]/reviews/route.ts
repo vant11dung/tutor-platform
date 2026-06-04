b49eef77be5200cd193fc37149ee6c684b337c8d
vant11dung <vantiendung08012007@gmail.com>
2026-06-05 00:50:51 +0700

    fix: update nextjs 15 dynamic params

--- app/api/tutors/[id]/reviews/route.ts
+++ app/api/tutors/[id]/reviews/route.ts
@@ -3,7 +3,8 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { prisma } from "@/lib/prisma"
 
-export async function POST(req: Request, { params }: { params: { id: string } }) {
+export async function POST(req: Request, props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   try {
     const session = await getServerSession(authOptions);
     if (!session || (session.user as any).role !== "STUDENT") {
